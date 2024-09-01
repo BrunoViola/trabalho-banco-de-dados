7ee6499458af3cf6d45ceedc1c006d7a4589e9ce
Andre Muraoka <andmuraoka@gmail.com>
2024-09-01 15:26:04 -0300

    Alteração: simulação remoção de editora

--- Script simulação compra e alteração.sql
+++ Script simulação compra e alteração.sql
@@ -26,4 +26,20 @@ SELECT * FROM livraria.Possui WHERE Num_Nota_Fiscal_Compra = '1234567800012345';
 UPDATE livraria.Livro SET Preco = '60.00' WHERE ISBN = '9786586551525';
 SELECT * FROM livraria.Livro WHERE ISBN = '9786586551525';
 
+-- Alterando descrição de um livro
+UPDATE livraria.Livro 
+SET Descricao = 'Por que os grandes gurus do Vale do Silício proíbem seus filhos de usar telas? Você sabia que nunca na história da humanidade houve um declínio tão acentuado nas habilidades cognitivas? Você sabia que apenas trinta minutos por dia na frente de uma tela são suficientes para que o desenvolvimento intelectual da criança comece a ser afetado?
 
+O uso da tecnologia digital - smartphones, computadores, tablets, etc. - pelas novas gerações tem sido absolutamente astronômico. Para crianças de 2 a 8 anos de idade, o consumo médio é de cerca de três horas por dia. Entre 8 e 12 anos, a média diária gira em torno de cinco horas. Na adolescência, esse número sobe para quase sete horas, o que significa mais de 2.400 horas por ano, em plena fase de desenvolvimento intelectual.
+
+Ao contrário do que a imprensa e a indústria da tecnologia costumam difundir, o uso das telas, longe de ajudar no desenvolvimento de crianças e estudantes, acarreta sérios malefícios à saúde do corpo (obesidade, problemas cardiovasculares, expectativa de vida reduzida), ao estado emocional (agressividade, depressão, comportamentos de risco) e ao desenvolvimento intelectual (empobrecimento da linguagem, dificuldade de concentração e memória).
+
+O neurocientista Michel Desmurget, diretor de pesquisa do Instituto Nacional de Saúde da França, propõe a primeira síntese de vários estudos que confirmaram os perigos reais das telas e nos alerta para as graves consequências de continuarmos a promover sem senso crítico o uso dessas tecnologias. ' 
+WHERE ISBN = '9786586551525';
+SELECT * FROM livraria.Livro WHERE ISBN = '9786586551525';
+
+--Deletando uma editora (causando a remoção de livros, mas não deleta o ISBN do Possui)
+DELETE FROM livraria.Editora WHERE Nome = 'Vestígio';
+SELECT * FROM livraria.editora ORDER BY id ASC;
+
+SELECT * FROM livraria.Possui WHERE Num_Nota_Fiscal_Compra = '1234567800012345';
