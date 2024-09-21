f5c9f2ece7ce66ffbdac160912484dd7e656090a
Andre Muraoka <andmuraoka@gmail.com>
2024-09-21 19:59:23 -0300

    Adição: model dos relacionamendos N:N

---
+++ Livraria/src/main/java/uel/br/Livraria/Model/Escrito.java
@@ -0,0 +1,22 @@
+package uel.br.Livraria.Model;
+
+public class Escrito {
+    Livro livro;
+    Autor autor;
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+}
---
+++ Livraria/src/main/java/uel/br/Livraria/Model/Pertence.java
@@ -0,0 +1,22 @@
+package uel.br.Livraria.Model;
+
+public class Pertence {
+    Livro livro;
+    Genero genero;
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+}
