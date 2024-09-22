a0936a9a9fb3be32c79f4a16be606af60ddcadb2
Andre Muraoka <andmuraoka@gmail.com>
2024-09-22 18:05:20 -0300

    Adição: PossuiController

--- Livraria/src/main/java/uel/br/Livraria/Controller/PossuiController.java
+++ Livraria/src/main/java/uel/br/Livraria/Controller/PossuiController.java
@@ -5,10 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-import uel.br.Livraria.DAO.DAO;
-import uel.br.Livraria.DAO.DAOFactory;
-import uel.br.Livraria.DAO.PgPossuiDAO;
+import uel.br.Livraria.DAO.*;
 
+import uel.br.Livraria.JDBC.PgConnectionFactory;
 import uel.br.Livraria.Model.Possui;
 import uel.br.Livraria.Model.Compra;
 import uel.br.Livraria.Model.Livro;
@@ -20,21 +19,13 @@ import java.sql.SQLException;
 public class PossuiController {
     private final PgPossuiDAO pgPossuiDAO;
 
-
     public PossuiController(PgPossuiDAO pgPossuiDAO) {
         this.pgPossuiDAO = pgPossuiDAO;
     }
 
     @PostMapping
     public ResponseEntity<String> criarPossui(@RequestBody Possui possui) {
-        try {   
-            Compra compra = new Compra();
-            compra.setNum_Nota_Fiscal(possui.getCompra().getNum_Nota_Fiscal()); // Supondo que essa seja a chave no JSON
-            possui.setCompra(compra);
-            
-            Livro livro = new Livro();
-            livro.setISBN(possui.getLivro().getISBN());
-            possui.setLivro(livro);
+        try {
             pgPossuiDAO.create(possui);
             return ResponseEntity.ok("Relacionamento 'possui' criado com sucesso!");
         } catch (SQLException e) {
@@ -42,8 +33,9 @@ public class PossuiController {
         }
     }
 
-    @GetMapping("/compra/{numNotaFiscal}/livro/{ISBN}")
+    @GetMapping("/{numNotaFiscal}/{ISBN}")
     public ResponseEntity<Possui> obterPossui(@PathVariable("numNotaFiscal") Long numNotaFiscal, @PathVariable("ISBN") Long ISBN) {
+
         try {
             Possui possui = pgPossuiDAO.read(numNotaFiscal, ISBN);
             if (possui != null) {
@@ -54,42 +46,24 @@ public class PossuiController {
         } catch (SQLException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
-      }
+    }
     
-      @PutMapping("/compra/{numNotaFiscal}/livro/{ISBN}")
-      public ResponseEntity<String> atualizarPossui(
-              @PathVariable("numNotaFiscal") Long numNotaFiscal,
-              @PathVariable("ISBN") Long ISBN,
-              @RequestBody Possui possui) {
-          try (DAOFactory daoFactory = DAOFactory.getInstance()) {
-              DAO<Compra, Long> compraDAO = daoFactory.getCompraDAO();
-              Compra compra = compraDAO.read(numNotaFiscal);
-              
-              if (compra == null) {
-                  return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Compra não encontrada.");
-              }
-      
-              DAO<Livro, Long> livroDAO = daoFactory.getLivroDAO();
-              Livro livro = livroDAO.read(ISBN);
-              
-              if (livro == null) {
-                  return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Livro não encontrado.");
-              }
-      
-              possui.setCompra(compra);
-              possui.setLivro(livro);
-      
-              pgPossuiDAO.update(possui);
-              return ResponseEntity.ok("Atualizado com sucesso");
-          } catch (SQLException e) {
-              return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar");
-          } catch (Exception e) {
-              return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado");
-          }
+    @PutMapping("/{numNotaFiscal}/{ISBN}")
+    public ResponseEntity<String> atualizarPossui(
+      @PathVariable("numNotaFiscal") Long numNotaFiscal,
+      @PathVariable("ISBN") Long ISBN,
+      @RequestBody Possui possui) {
+      try {
+          pgPossuiDAO.update(possui);
+          return ResponseEntity.ok("Atualizado com sucesso");
+      } catch (SQLException e) {
+          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao atualizar");
+      } catch (Exception e) {
+          return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro inesperado");
       }
-    
+    }
 
-    @DeleteMapping("/compra/{numNotaFiscal}/livro/{ISBN}")
+    @DeleteMapping("/{numNotaFiscal}/{ISBN}")
     public ResponseEntity<String> deletarSecao(@PathVariable("numNotaFiscal") Long numNotaFiscal, @PathVariable("ISBN") Long ISBN) {
         try {
             pgPossuiDAO.delete(numNotaFiscal, ISBN);
