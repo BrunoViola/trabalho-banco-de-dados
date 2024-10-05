58bdd457e2372773f2db066bc41277241682d820
BrunoViola <brunohenriquesilvaviola@gmail.com>
2024-10-05 18:40:33 -0300

    Correção: consulta determinando as faixas de preço por meio de um CASE

--- SQL/Select Relatórios/Vendas por Preço Médio.sql
+++ SQL/Select Relatórios/Vendas por Preço Médio.sql
@@ -1,10 +1,16 @@
 -- Esta consulta retorna dados acerca da quantidade de livros vendidos por faixa de preço
--- 
-SELECT AVG(l.Preco) AS Preco_Medio,
-       SUM(ps.Quantidade) AS Unidades_Vendidas
+SELECT 
+    CASE 
+        WHEN l.Preco < 10 THEN '0 - 9.99'
+        WHEN l.Preco < 30 THEN '10 - 29.99'
+        WHEN l.Preco < 50 THEN '30 - 49.99'
+        WHEN l.Preco < 100 THEN '50 - 99.99'
+        ELSE '100 e acima'
+    END AS Faixa_Preco,
+    SUM(ps.Quantidade) AS Unidades_Vendidas
 FROM livraria.Possui ps
 JOIN livraria.Livro l ON ps.ISBN_Livro = l.ISBN
 JOIN livraria.Pertence p ON l.ISBN = p.ISBN_Livro
 JOIN livraria.Genero g ON p.ID_Genero = g.ID
-GROUP BY l.ISBN
-ORDER BY Preco_Medio ASC;
+GROUP BY Faixa_Preco
+ORDER BY Faixa_Preco;
