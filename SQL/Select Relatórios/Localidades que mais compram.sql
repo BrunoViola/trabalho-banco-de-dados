1f54266cc4fade08720a7394d41618181ade1ede
BrunoViola <brunohenriquesilvaviola@gmail.com>
2024-10-06 14:02:06 -0300

    Correção: pequenas correções de nomenclaturas e concatenações

--- SQL/Select Relatórios/Localidades que mais compram.sql
+++ SQL/Select Relatórios/Localidades que mais compram.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(cl.Cidade, ' ', cl.Estado) AS Localizacao, SUM(cp.Total) AS Total_Gasto
+SELECT CONCAT(cl.Cidade, ' - ', cl.Estado) AS Localizacao, SUM(cp.Total) AS Total_Gasto
 FROM livraria.Cliente cl
 JOIN livraria.Compra cp ON cp.CPF_Cliente = cl.CPF
 WHERE cp.Data_Compra BETWEEN '2023-09-29' AND '2024-10-06'
--- SQL/Select Relatórios/Vendas por Preço Médio.sql
+++ SQL/Select Relatórios/Vendas por Preço Médio.sql
@@ -1,5 +1,4 @@
 -- Esta consulta retorna dados acerca da quantidade de livros vendidos por faixa de preço
--- Esta consulta retorna dados acerca da quantidade de livros vendidos por faixa de preço
 WITH faixas AS (
     SELECT '0 - 9.99' AS Faixa_Preco, 0 AS Unidades_Vendidas
     UNION ALL
@@ -12,17 +11,17 @@ WITH faixas AS (
     SELECT 'acima de 100', 0
 )
 SELECT
-	f.Faixa_Preco,
+	fxs.Faixa_Preco,
 	COALESCE(SUM(ps.Quantidade), 0) AS Unidades_Vendidas
-FROM faixas f
+FROM faixas fxs
 LEFT JOIN livraria.Livro l ON
     (CASE 
-        WHEN f.Faixa_Preco = '0 - 9.99' THEN l.Preco <= 9.99
-        WHEN  f.Faixa_Preco ='10 - 29.99' THEN l.Preco BETWEEN 10 AND 29.99
-        WHEN  f.Faixa_Preco ='30 - 49.99' THEN l.Preco BETWEEN 30 AND 49.99
-        WHEN  f.Faixa_Preco ='50 - 99.99' THEN l.Preco BETWEEN 50 AND 99.99
+        WHEN fxs.Faixa_Preco = '0 - 9.99' THEN l.Preco <= 9.99
+        WHEN fxs.Faixa_Preco ='10 - 29.99' THEN l.Preco BETWEEN 10 AND 29.99
+        WHEN fxs.Faixa_Preco ='30 - 49.99' THEN l.Preco BETWEEN 30 AND 49.99
+        WHEN fxs.Faixa_Preco ='50 - 99.99' THEN l.Preco BETWEEN 50 AND 99.99
         ELSE l.preco >= 100
     END)
-JOIN livraria.Possui ps ON ps.ISBN_Livro = l.ISBN
+LEFT JOIN livraria.Possui ps ON ps.ISBN_Livro = l.ISBN
 GROUP BY Faixa_Preco
 ORDER BY Faixa_Preco;
