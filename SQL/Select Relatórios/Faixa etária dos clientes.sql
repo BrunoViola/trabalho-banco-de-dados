324b6bc7ab5770dbb321c0f47ccb39a9fcbefd02
BrunoViola <brunohenriquesilvaviola@gmail.com>
2024-10-05 22:31:53 -0300

    Adição: duas novas consultas para geração de relatórios

---
+++ SQL/Select Relatórios/Faixa etária dos clientes.sql
@@ -0,0 +1,32 @@
+-- Aqui será obtidas as faixas de idades dos clientes de maneira bem dividida
+WITH faixas AS (
+    SELECT 'Menos de 14 anos' AS Faixa_Etaria, 0 AS Quantidade
+    UNION ALL
+    SELECT '14-17 anos', 0
+    UNION ALL
+    SELECT '18-24 anos', 0
+    UNION ALL
+    SELECT '25-44 anos', 0
+    UNION ALL
+    SELECT '45-64 anos', 0
+    UNION ALL
+    SELECT '65-74 anos', 0
+    UNION ALL
+    SELECT '75 anos ou mais', 0
+)
+SELECT 
+    fxs.Faixa_Etaria,
+    COALESCE(COUNT(c.CPF), 0) AS Quantidade
+FROM faixas fxs
+LEFT JOIN livraria.Cliente c ON 
+    (CASE 
+        WHEN fxs.Faixa_Etaria = 'Menos de 14 anos' THEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, c.data_nascimento)) < 14
+        WHEN fxs.Faixa_Etaria = '14-17 anos' THEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, c.data_nascimento)) BETWEEN 14 AND 17
+        WHEN fxs.Faixa_Etaria = '18-24 anos' THEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, c.data_nascimento)) BETWEEN 18 AND 24
+        WHEN fxs.Faixa_Etaria = '25-44 anos' THEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, c.data_nascimento)) BETWEEN 25 AND 44
+        WHEN fxs.Faixa_Etaria = '45-64 anos' THEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, c.data_nascimento)) BETWEEN 45 AND 64
+        WHEN fxs.Faixa_Etaria = '65-74 anos' THEN EXTRACT(YEAR FROM AGE(CURRENT_DATE, c.data_nascimento)) BETWEEN 65 AND 74
+        ELSE EXTRACT(YEAR FROM AGE(CURRENT_DATE, c.data_nascimento)) >= 75
+    END)
+GROUP BY fxs.Faixa_Etaria
+ORDER BY fxs.Faixa_Etaria;
---
+++ SQL/Select Relatórios/Localidades que mais compram.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(cl.Cidade, ' ', cl.Estado) AS Localizacao, SUM(cp.Total) AS Total_Gasto
+FROM livraria.Cliente cl
+JOIN livraria.Compra cp ON cp.CPF_Cliente = cl.CPF
+WHERE cp.Data_Compra BETWEEN '2023-09-29' AND '2024-10-06'
+GROUP BY Localizacao
+HAVING SUM(cp.Total)>90
+ORDER BY Total_Gasto DESC;
