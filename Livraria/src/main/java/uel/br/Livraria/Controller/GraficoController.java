ce3703e30c1ade1e0598e016f50288c2878d1366
Andre Muraoka <andmuraoka@gmail.com>
2024-10-06 19:34:18 -0300

    Alteração: CrossOrigin de GraficoController

--- Livraria/src/main/java/uel/br/Livraria/Controller/GraficoController.java
+++ Livraria/src/main/java/uel/br/Livraria/Controller/GraficoController.java
@@ -2,6 +2,7 @@ package uel.br.Livraria.Controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/dados")
+@CrossOrigin(origins = "http://localhost:3000")
 public class GraficoController {
     private final PgGraficoDAO pgGraficoDAO;
 
