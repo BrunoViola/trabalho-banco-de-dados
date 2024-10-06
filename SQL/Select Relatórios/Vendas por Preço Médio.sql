55da9d53542f5946808bd7d46c9ba771fa03df74
BrunoViola <brunohenriquesilvaviola@gmail.com>
2024-10-05 23:39:26 -0300

    Correção: a consulta de vendas por intervalos de preços apresentava erros, foi realizada uma correção com a aplciação de uma estratégia semelhante ao cálculo da faixa etária

--- SQL/Select Relatórios/Vendas por Preço Médio.sql
+++ SQL/Select Relatórios/Vendas por Preço Médio.sql
@@ -1,16 +1,28 @@
 -- Esta consulta retorna dados acerca da quantidade de livros vendidos por faixa de preço
-SELECT 
-    CASE 
-        WHEN l.Preco < 10 THEN '0 - 9.99'
-        WHEN l.Preco < 30 THEN '10 - 29.99'
-        WHEN l.Preco < 50 THEN '30 - 49.99'
-        WHEN l.Preco < 100 THEN '50 - 99.99'
-        ELSE '100 e acima'
-    END AS Faixa_Preco,
-    SUM(ps.Quantidade) AS Unidades_Vendidas
-FROM livraria.Possui ps
-JOIN livraria.Livro l ON ps.ISBN_Livro = l.ISBN
-JOIN livraria.Pertence p ON l.ISBN = p.ISBN_Livro
-JOIN livraria.Genero g ON p.ID_Genero = g.ID
+-- Esta consulta retorna dados acerca da quantidade de livros vendidos por faixa de preço
+WITH faixas AS (
+    SELECT '0 - 9.99' AS Faixa_Preco, 0 AS Unidades_Vendidas
+    UNION ALL
+    SELECT '10 - 29.99', 0
+    UNION ALL
+    SELECT '30 - 49.99', 0
+    UNION ALL
+    SELECT '50 - 99.99', 0
+    UNION ALL
+    SELECT 'acima de 100', 0
+)
+SELECT
+	f.Faixa_Preco,
+	COALESCE(SUM(ps.Quantidade), 0) AS Unidades_Vendidas
+FROM faixas f
+LEFT JOIN livraria.Livro l ON
+    (CASE 
+        WHEN f.Faixa_Preco = '0 - 9.99' THEN l.Preco <= 9.99
+        WHEN  f.Faixa_Preco ='10 - 29.99' THEN l.Preco BETWEEN 10 AND 29.99
+        WHEN  f.Faixa_Preco ='30 - 49.99' THEN l.Preco BETWEEN 30 AND 49.99
+        WHEN  f.Faixa_Preco ='50 - 99.99' THEN l.Preco BETWEEN 50 AND 99.99
+        ELSE l.preco >= 100
+    END)
+JOIN livraria.Possui ps ON ps.ISBN_Livro = l.ISBN
 GROUP BY Faixa_Preco
 ORDER BY Faixa_Preco;
