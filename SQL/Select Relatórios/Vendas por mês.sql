43d20fff11c558def2de1f24fdfaaa74e21ec1eb
BrunoViola <brunohenriquesilvaviola@gmail.com>
2024-10-07 09:43:07 -0300

    Correção: os meses estavam sendo ordenados por ordem alfabética

--- SQL/Select Relatórios/Vendas por mês.sql
+++ SQL/Select Relatórios/Vendas por mês.sql
@@ -3,6 +3,6 @@ SELECT TO_CHAR(c.data_compra, 'Month') AS mes,
     SUM(p.Preco * p.Quantidade) AS Total_Vendas
 FROM livraria.Compra c
 JOIN livraria.Possui p ON c.Num_Nota_Fiscal = p.Num_Nota_Fiscal_Compra
-GROUP BY mes
-ORDER BY mes DESC
+GROUP BY c.data_compra
+ORDER BY EXTRACT ('Month' From c.data_compra) ASC
 LIMIT 6;
